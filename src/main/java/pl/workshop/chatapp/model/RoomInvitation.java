ce9a186509679cc74757dcaad6087a0c08543efc
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2026-03-30 01:23:47 +0200

    Create RoomInvitation.java

---
+++ src/main/java/pl/workshop/chatapp/model/RoomInvitation.java
@@ -0,0 +1,31 @@
+package pl.workshop.chatapp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "room_invitations")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class RoomInvitation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
+
+    @ManyToOne
+    @JoinColumn(name = "inviter_id", nullable = false)
+    private User inviter;
+
+    @ManyToOne
+    @JoinColumn(name = "invitee_id", nullable = false)
+    private User invitee;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private boolean accepted = false;
+}
