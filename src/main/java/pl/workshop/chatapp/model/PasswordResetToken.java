1233c422d5e8850519b300224d9491995b38b84e
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2026-03-30 01:22:58 +0200

    Create PasswordResetToken.java

---
+++ src/main/java/pl/workshop/chatapp/model/PasswordResetToken.java
@@ -0,0 +1,25 @@
+package pl.workshop.chatapp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "password_reset_tokens")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class PasswordResetToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String token;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private LocalDateTime expiryDate = LocalDateTime.now().plusHours(24);
+}
