cdf58af108c641082236b424d0ff9a979c6cafd7
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2026-03-30 00:40:42 +0200

    Update WebSocketConfig.java

--- src/main/java/pl/workshop/chatapp/config/WebSocketConfig.java
+++ src/main/java/pl/workshop/chatapp/config/WebSocketConfig.java
@@ -1,30 +1,39 @@
 package pl.workshop.chatapp.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import pl.workshop.chatapp.security.WebSocketJwtInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private final WebSocketJwtInterceptor jwtInterceptor;
+
+    public WebSocketConfig(WebSocketJwtInterceptor jwtInterceptor) {
+        this.jwtInterceptor = jwtInterceptor;
+    }
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");           // pokoje publiczne/prywatne
+        config.enableSimpleBroker("/topic", "/queue");        // pokoje i presence
         config.setApplicationDestinationPrefixes("/app");
+        config.setUserDestinationPrefix("/user");
     }
-    
-    @Override
-    public void configureClientInboundChannel(org.springframework.messaging.support.ChannelRegistration registration) {
-        registration.interceptors(new WebSocketJwtInterceptor(jwtService));
-    }
-    
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat")
                 .setAllowedOriginPatterns("*")
                 .withSockJs();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(jwtInterceptor);
     }
 }
