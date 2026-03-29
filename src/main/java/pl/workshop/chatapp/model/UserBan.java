7edf62478351b81ad72b38d31c25005bb9ac71b2
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2026-03-30 01:10:47 +0200

    Create UserBan.java

---
+++ src/main/java/pl/workshop/chatapp/model/UserBan.java
@@ -0,0 +1,26 @@
+package com.example.chatserver.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_bans")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class UserBan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "banner_id", nullable = false)
+    private User banner;     // kto zbanował
+
+    @ManyToOne
+    @JoinColumn(name = "banned_id", nullable = false)
+    private User banned;     // kogo zbanował
+
+    private LocalDateTime bannedAt = LocalDateTime.now();
+}
