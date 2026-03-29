fbceede9a33eeb3988a4cc06c293000207f4d27c
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2026-03-30 01:12:22 +0200

    Update FriendRequestRepository.java

--- src/main/java/pl/workshop/chatapp/repository/FriendRequestRepository.java
+++ src/main/java/pl/workshop/chatapp/repository/FriendRequestRepository.java
@@ -1,12 +1,13 @@
-package pl.workshop.chatapp.repository;
+package com.example.chatserver.repository;
 
+import com.example.chatserver.model.FriendRequest;
+import com.example.chatserver.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import pl.workshop.chatapp.model.FriendRequest;
-import pl.workshop.chatapp.model.User;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long> {
-    List<FriendRequest> findByReceiverAndStatus(User receiver, FriendRequestStatus status);
+    List<FriendRequest> findByReceiverAndStatus(User receiver, FriendRequest.FriendRequestStatus status);
     Optional<FriendRequest> findBySenderAndReceiver(User sender, User receiver);
 }
