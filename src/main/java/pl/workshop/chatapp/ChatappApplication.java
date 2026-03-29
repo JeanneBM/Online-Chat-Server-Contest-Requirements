a3ddaa9a996892959d7286e891235340a82465db
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2026-03-29 23:48:04 +0200

    Update ChatappApplication.java

--- src/main/java/pl/workshop/chatapp/ChatappApplication.java
+++ src/main/java/pl/workshop/chatapp/ChatappApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatappApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ChatappApplication.class, args);
-	}
-
+public class ChatAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatAppApplication.class, args);
+    }
 }
