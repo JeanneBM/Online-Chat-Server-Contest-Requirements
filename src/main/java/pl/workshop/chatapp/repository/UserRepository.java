ab14aebab1e80c3262f510d3e74f72aeca759d5b
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2026-03-30 02:02:02 +0200

    Update UserRepository.java

--- src/main/java/pl/workshop/chatapp/repository/UserRepository.java
+++ src/main/java/pl/workshop/chatapp/repository/UserRepository.java
@@ -2,6 +2,7 @@ package pl.workshop.chatapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.workshop.chatapp.model.User;
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
